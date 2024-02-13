c771fdc7f49978b8696c8787509823082272fce9
Cirde <cirdemosquito04@gmail.com>
2024-02-13 19:14:18 +0800

    Numbering

--- src/main/kotlin/Activity1.kt
+++ src/main/kotlin/Activity1.kt
@@ -1,3 +1,27 @@
-package org.example
+fun main() {
 
-// My Name is Gabs //
+    for (numberOfGroup in 1..4) {
+    print("What is Your Name? ")
+    var name = readLine()
+
+    print("What is Your Middle Initial? ")
+    var middlename = readLine()
+
+    print("What is Your Last name? ")
+    var lastname = readLine()
+
+    print("What is Your Age? ")
+    var age = readLine()?.toInt() ?:0
+
+    println("Your Name is $name $middlename. $lastname")
+    println("$age years old")
+
+    }
+
+}
+
+
+
+
+
+
