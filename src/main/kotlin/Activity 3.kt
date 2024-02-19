9711e6728ae73ad3e9b240485e5b13c07a9fe0b7
Luna <159744316+Cirdie@users.noreply.github.com>
2024-02-19 18:50:19 +0800

    Update Activity 3.kt

--- src/main/kotlin/Activity 3.kt
+++ src/main/kotlin/Activity 3.kt
@@ -33,3 +33,30 @@ fun main() {
         println("$name belongs to the $ageGroup group.")
     }
 }
+
+//Other Method
+
+/*
+
+fun main() {
+    println("Enter the number of group members:")
+    val numOfMembers = readLine()!!.toInt()
+
+    repeat(numOfMembers) {
+        println("Enter name:")
+        val name = readLine()!!
+        println("Enter age:")
+        val age = readLine()!!.toInt()
+
+        val ageGroup = when {
+            age in 1..17 -> "underage"
+            age in 18..30 -> "young adult"
+            age in 31..59 -> "adult"
+            age >= 60 -> "senior"
+            else -> "Invalid age"
+        }
+
+        println("$name belongs to the $ageGroup group.")
+    }
+}
+*/
